8dd5dd3653d9a76b7deda415037d5382ca06266b
Arnav-Bansal2710 <bansalarnavab@gmail.com>
2026-07-24 18:47:43 +0530

    Configure SSL connection for TiDB Cloud

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,31 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host:             process.env.DB_HOST,
+//   user:             process.env.DB_USER,
+//   password:         process.env.DB_PASSWORD,
+//   database:         process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit:  10,
+// });
+
+// const db = pool.promise();
+// module.exports = db;
+
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host:             process.env.DB_HOST,
-  user:             process.env.DB_USER,
-  password:         process.env.DB_PASSWORD,
-  database:         process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit:  10,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true
+  }
 });
 
-const db = pool.promise();
-module.exports = db;
+module.exports = pool.promise();
